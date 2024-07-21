e01372026ed49103e139adde50a892f0b6e5d376
Connor S <ohvconnor@gmail.com>
2024-07-20 19:27:02 -0700

    temp

--- src/render.tsx
+++ src/render.tsx
@@ -1,5 +1,16 @@
 import Cube from "./cube";
 
+interface Coord2D {
+    x: number;
+    y: number;
+};
+
+interface Coord3D {
+    x: number;
+    y: number;
+    z: number;
+};
+
 export default class Renderer {
     private view_height: number;
     private view_width: number;
@@ -11,18 +22,24 @@ export default class Renderer {
 
     public buildNextFrame(): string {
         let frame = "";
-        let origin = [this.view_width / 2, this.view_height / 2];
+        let origin: Coord2D = { x: Math.round(this.view_width / 2), y: Math.round(this.view_height / 2) };
+
+        console.log(origin);
 
         let vertices = [
             [0, 0], [1, 0],
             [1, 1], [0, 1]
         ];
 
-        for (let i = 0; i < this.view_width; i++) {
-            for (let j = 0; j < this.view_height; j++) {
-                if (origin[])
-                    frame += " ";
+        for (let y = 0; y < this.view_height; y++) {
+            for (let x = 0; x < this.view_width; x++) {
+                if (origin.x === x && origin.y === y)
+                    frame += "x";
+                else
+                    frame += "-";
             }
+
+            frame += "\n";
         }
 
         return frame;
