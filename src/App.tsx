bc75127b22c1b5d1c26c5f8ffb0ac39bd8f23cfb
Connor S <ohvconnor@gmail.com>
2024-07-20 19:07:52 -0700

    temp commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
+import Renderer from './render';
 
 function App() {
-  const [cube, setCube] = useState<string>(""); //63x31
+  const [cube, setCube] = useState<string>("");
+
+  let renderer = new Renderer(63, 31);
+
+  useEffect(() => {
+    setCube(renderer.buildNextFrame());
+    //const interval = setInterval(() => setCube(renderer.renderFrame()), 10000);
+    return () => {
+      //clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className="App">
--- src/render.tsx
+++ src/render.tsx
@@ -1,1 +1,30 @@
-export { }
+import Cube from "./cube";
+
+export default class Renderer {
+    private view_height: number;
+    private view_width: number;
+
+    constructor(width: number, height: number) {
+        this.view_width = width;
+        this.view_height = height;
+    }
+
+    public buildNextFrame(): string {
+        let frame = "";
+        let origin = [this.view_width / 2, this.view_height / 2];
+
+        let vertices = [
+            [0, 0], [1, 0],
+            [1, 1], [0, 1]
+        ];
+
+        for (let i = 0; i < this.view_width; i++) {
+            for (let j = 0; j < this.view_height; j++) {
+                if (origin[])
+                    frame += " ";
+            }
+        }
+
+        return frame;
+    }
+}
